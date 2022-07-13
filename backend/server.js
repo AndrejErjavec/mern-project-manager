ad821502162a6c4ad01c39f4c7f09d6c12348e77
unknown <erandrej@gmail.com>
2022-07-13 19:07:16 +0200

    database connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
 const port = process.env.PORT || 5000
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('hello'))
-
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, (err) => {
+  if (err) console.log(err)
+  console.log(`Server started on port ${port}`)
+})
