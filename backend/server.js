7c1c1d0d83e001be19682d22104a9fd266af09e7
AndrejErjavec <erandrej@gmail.com>
2022-08-07 12:18:30 +0200

    server preparation for deployment

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,15 @@ app.use('/api/task', require('./routes/taskRoutes'));
 app.use('/api/subtask', require('./routes/subtaskRoutes'));
 app.use('/api/comment', require('./routes/commentRoutes'));
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/', 'build', 'index.html')));
+}
+else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
+
 app.use(errorHandler);
 
 app.listen(port, (err) => {
